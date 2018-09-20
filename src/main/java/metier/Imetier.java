76d1ef0a8abaeb7bb9cb0d1ddf810aff298d19fb
gk <gk@172.16.3.59>
2018-09-20 10:18:39 +0200

    SECOND COMMIT

--- src/main/java/metier/Imetier.java
+++ src/main/java/metier/Imetier.java
@@ -1,5 +1,23 @@
 package metier;
 
+
+import java.util.List;
+
+import entities.Agent;
+import entities.Client;
+import entities.Compte;
+import entities.Transaction;
+
 public interface Imetier {
- //klkksss
+	
+	
+	/*client*/
+	public Client ajouterClient(Client c);
+	public Client consulterClient(Long codeCli);
+	
+
+	
 }
+
+	
+
